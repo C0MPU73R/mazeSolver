c09365a9cbc32ced30f0e4772710dda1099228a7
Mad V <55332110+madv@users.noreply.github.com>
2019-09-26 17:48:30 -0400

    Added file input

--- mazeSolver/mazeSolver/Source.cpp
+++ mazeSolver/mazeSolver/Source.cpp
@@ -1,18 +1,42 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "mazeSolver.h"
 using namespace std;
 int main()
 {
-	vector<vector<char>> mazeFromFile = { {'S', 'O', 'W'},
-										  {'O', 'O', 'W'} ,
-										  {'E', 'W', 'W',} };
-										  
-	mazeSolver a(mazeFromFile);
-	int startXLoc = a.findStartX();
-	int startYLoc = a.findStartY();
-	int min = a.shortestPath(startXLoc, startYLoc);
-	cout << min << endl;
-	a.printMaze();
+	vector<string> linesFromFile;
+	ifstream file;
+	string fileName;
+	cout << "Enter file name: ";
+	cin >> fileName;
+	file.open(fileName);
+	if (file.is_open())
+	{
+		while (!file.eof())
+		{
+			getline(file, fileName);
+			linesFromFile.push_back(fileName);
+		}
+	}
+	file.close();
+	for (int index = 0; index < linesFromFile.size(); index++)
+	{
+		cout << linesFromFile[index] << endl;
+	}
+	//test
+
+
+
+
+
+
+	//mazeSolver a(mazeFromFile);
+	//int startXLoc = a.findStartX();
+	//int startYLoc = a.findStartY();
+	//int min = a.shortestPath(startXLoc, startYLoc);
+	//cout << min << endl;
+	//a.printMaze();
 
 
 
